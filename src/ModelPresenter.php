72f6c5a572d2f9470dcf4b3bd05527cbcd70a0da
Sergey Milimko <smilimko@gmail.com>
2016-01-15 21:00:22 +0500

    Comment code.

--- src/ModelPresenter.php
+++ src/ModelPresenter.php
@@ -1,11 +1,34 @@
 <?php
+/**
+ * Copyright (c) 2016 Aleph Tav
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
+ * and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO 
+ * THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * @author Aleph Tav <4lephtav@gmail.com>
+ * @copyright Copyright &copy; 2016 Aleph Tav
+ * @license http://www.opensource.org/licenses/MIT
+ */
 
 namespace AlephTools\Data\Presenters;
 
 use Illuminate\Database\Eloquent\Model;
 
 /**
- * Used to present some model info.
+ * Used to present some Eloquent model.
+ *
+ * @author Aleph Tav <4lephtav@gmail.com>
+ * @version 1.0.0
+ * @package alephtools.data.presenters
  */
 class ModelPresenter extends ArrayPresenter
 {
